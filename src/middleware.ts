51a8854cd3ee3b41defef00478c7b8991237d3d6
Werner Traut <wtraut@collineargroup.com>
2024-11-27 09:53:27 -0700

    token

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
+  console.log(process.env.AUTH_SECRET);
+
   const token = await getToken({ req, secret: process.env.AUTH_SECRET });
   const isLoggedIn = !!token;
   console.log("isLoggedIn", isLoggedIn, token);

217778857357ea59e33d83af5746832f516997ff
Werner Traut <wtraut@collineargroup.com>
2026-02-16 14:15:35 -0700

    feat: Prioritize DIRECT_URL for Prisma connection string.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = `${process.env.DIRECT_URL || process.env.DATABASE_URL}`;
 
 const prismaClientSingleton = () => {
   const pool = new Pool({
